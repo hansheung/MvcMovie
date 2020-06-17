faaa15659e502ba0ac2da244ceed700c3615a24a
Foxtrot_M_2020 <hansheung@gmail.com>
2020-06-17 15:47:54 +0800

    imi

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -21,7 +21,7 @@ namespace MvcMovie.Controllers
 
         public string Welcome(string name, int ID = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            return HttpUtility.HtmlEncode("Hello Change something " + name + ", ID: " + ID);
         }
     }
 }
